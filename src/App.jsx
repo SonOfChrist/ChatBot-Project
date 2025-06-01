c4babe04029eeea6944b29459711c4736e39e86f
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-06-01 13:20:52 +0300

    Chat-Bot-Project

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState} from 'react'
+import { ChatInput } from './components/ChatInput';
+import ChatMessages from './components/ChatMessages';
+import './App.css'
+
+function App() {
+  const [chatMessages, setChatMessages] = useState([{
+      message: 'hello chatbot',
+      sender: 'userprofileimage',
+      id: 'id1'
+  }, {
+      message: 'Hello! How can i help you',
+      sender: 'robotprofileimage',
+      id: 'id2'
+  }, {
+      message: 'can you get me Todays Date?',
+      sender: 'userprofileimage',
+      id: 'id3'
+  }, {
+      message: 'Today is MAY 14',
+      sender: 'robotprofileimage',
+      id: 'id4'
+  }]);
+
+  return (
+      <div className="app-container">
+          < ChatMessages chatMessages = {chatMessages} />
+          < ChatInput chatMessages={chatMessages} setChatMessages={setChatMessages} />
+      </div>
+  );
+};
+
+export default App
---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,14 @@
+import robotProfileImage from '../assets/robotprofileimage.png';
+import userProfileImage from '../assets/userprofileimage.png';
+import './ChatMessage.css';
+
+export function ChatMessage({message, sender}) {
+return(
+      <div className={sender === 'userprofileimage' ? 'chat-message-userprofileimage' : 'chat-message-robotprofileimage'}>
+          { sender === 'robotprofileimage' &&  <img src={robotProfileImage} alt="robotprofileimage" className="chat-message-profileimage" /> }
+          <div className="chat-message-text">{message}</div>
+          { sender === 'userprofileimage' &&  <img src={userProfileImage} alt="userprofileimage" className="chat-message-profileimage" /> }
+      </div>
+  );
+};
+
