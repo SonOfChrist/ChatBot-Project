7a6c94faa21c67339255f685a4a00a1b4d339a38
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-06-23 20:52:14 +0300

    Update on ChatBot

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -4,7 +4,11 @@ import './ChatMessage.css';
 
 export function ChatMessage({message, sender}) {
 return(
-      <div className= { sender === 'userprofileimage' ? 'chat-message-userprofileimage' : 'chat-message-robotprofileimage'}>
+      <div className= { 
+        sender === 'userprofileimage' 
+            ? 'chat-message-userprofileimage' 
+            : 'chat-message-robotprofileimage'
+        }>
           { sender === 'robotprofileimage' &&  <img src={robotProfileImage} className="chat-message-profileimage" /> }
           <div className="chat-message-text">{message}</div>
           { sender === 'userprofileimage' &&  <img src={userProfileImage} className="chat-message-profileimage" /> }
